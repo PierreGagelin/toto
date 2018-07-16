bea3a8026c2640b6260c74380ccc182584daa6f5
Pierre Gagelin <pierre.gagelin@gmail.com>
2018-07-16 20:02:55 +0200

    Forgot file...

---
+++ protobuf/tu_toto_pb.py
@@ -0,0 +1,15 @@
+#!/usr/local/bin/python3
+
+import sys
+
+# Protobuf generated file
+import toto_pb_pb2
+
+# Disable bytecode
+sys.dont_write_bytecode = 1
+
+message = toto_pb_pb2.toto_pb()
+
+print("{}".format(message.HasField("id1")))
+message.id1 = 1
+print("{}".format(message.HasField("id1")))
