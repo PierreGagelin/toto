0236118f19bf4d9d57ad91b36a28d3c432abb14b
Pierre Gagelin <pierre.gagelin@gmail.com>
2018-08-15 10:49:43 +0200

    Fixed git name and missing packages

--- env/package_install.sh
+++ env/package_install.sh
@@ -3,8 +3,8 @@
 sudo apt update
 sudo apt upgrade
 
-sudo apt install git cmake gcc g++ libboost-dev libprotobuf-dev protobuf-compiler libzmq3-dev python3-pip vim
+sudo apt install git cmake gcc g++ libboost-dev libprotobuf-dev protobuf-compiler libzmq3-dev python3-pip valgrind lcov vim gitk
 
 python3 -m pip install robotframework zmq protobuf
 
-# Installer visual studio code en allant télécharger le package
+# Visual studio code needs to be downloaded to be installed
