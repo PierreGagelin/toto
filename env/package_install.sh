936e56081d94799b3a99ec0219cc9394726dd4e6
Pierre Gagelin <pierre.gagelin@gmail.com>
2018-09-25 22:03:29 +0200

    Added bash completion package

--- env/package_install.sh
+++ env/package_install.sh
@@ -3,7 +3,7 @@
 sudo apt update
 sudo apt upgrade
 
-sudo apt install git cmake gcc g++ libboost-dev libprotobuf-dev protobuf-compiler libzmq3-dev python3-pip valgrind lcov vim gitk
+sudo apt install git cmake gcc g++ libboost-dev libprotobuf-dev protobuf-compiler libzmq3-dev python3-pip valgrind lcov vim gitk bash-completion
 
 python3 -m pip install robotframework zmq protobuf
 
