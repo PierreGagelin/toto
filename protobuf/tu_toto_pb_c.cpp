9bbe7221810aa8371e466502e5d3cac9ff1784da
Pierre Gagelin <pierre.gagelin@gmail.com>
2018-11-30 20:48:58 +0100

    Switched to proto3 and added oneof example

--- protobuf/tu_toto_pb_c.cpp
+++ protobuf/tu_toto_pb_c.cpp
@@ -9,15 +9,14 @@ extern "C"
 //
 // @brief Check descriptor and print a message
 //
-static void proto_print(TotoPb *msg)
+static void toto_pb_print(TotoPb *msg)
 {
     ASSERT(toto_pb__descriptor.fields[0].type == PROTOBUF_C_TYPE_ENUM);
-    ASSERT(toto_pb__descriptor.fields[0].label == PROTOBUF_C_LABEL_OPTIONAL);
+    ASSERT(toto_pb__descriptor.fields[0].label == PROTOBUF_C_LABEL_NONE);
     ASSERT(std::string(toto_pb__descriptor.fields[0].name) == "type");
-    ASSERT(msg->has_type != 0);
 
     ASSERT(toto_pb__descriptor.fields[1].type == PROTOBUF_C_TYPE_STRING);
-    ASSERT(toto_pb__descriptor.fields[1].label == PROTOBUF_C_LABEL_REQUIRED);
+    ASSERT(toto_pb__descriptor.fields[1].label == PROTOBUF_C_LABEL_NONE);
     ASSERT(std::string(toto_pb__descriptor.fields[1].name) == "toto");
 
     ASSERT(toto_pb__descriptor.fields[2].type == PROTOBUF_C_TYPE_INT32);
@@ -33,7 +32,7 @@ static void proto_print(TotoPb *msg)
     }
 }
 
-int main(int, char **)
+static void test_pack()
 {
     int32_t ids[] = {0, 2, 4, 6};
     char str[] = "hello world";
@@ -44,7 +43,6 @@ int main(int, char **)
     toto_pb__init(&input);
 
     input.type = TOTO_PB__TOTO_ENUM__TOTO_TWO;
-    input.has_type = 1;
     input.toto = str;
     input.ids = ids;
     input.n_ids = (sizeof(ids) / sizeof(ids[0]));
@@ -56,9 +54,40 @@ int main(int, char **)
 
     output = toto_pb__unpack(NULL, size, buffer);
 
-    proto_print(output);
+    toto_pb_print(output);
 
     toto_pb__free_unpacked(output, NULL);
+}
+
+static void test_oneof()
+{
+    TotoOneof input;
+    TotoOneof *output;
+    uint8_t buffer[256];
+    char hello[] = "hello world";
+
+    toto_oneof__init(&input);
+
+    input.msg_type_case = TOTO_ONEOF__MSG_TYPE_B;
+    input.b = hello;
+
+    size_t size = toto_oneof__get_packed_size(&input);
+    ASSERT(size <= sizeof(buffer));
+
+    toto_oneof__pack(&input, buffer);
+
+    output = toto_oneof__unpack(NULL, size, buffer);
+
+    ASSERT(output->msg_type_case == TOTO_ONEOF__MSG_TYPE_B);
+
+    toto_oneof__free_unpacked(output, NULL);
+}
+
+int main(int, char **)
+{
+    test_pack();
+
+    test_oneof();
 
     return 0;
 }
