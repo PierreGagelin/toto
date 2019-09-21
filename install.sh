0140f884c2bce94c2f04661fff8df3c92f0d7826
Pierre Gagelin <pierre.gagelin@gmail.com>
2019-09-21 11:18:41 +0200

    Updated docker install. Added uninstall script

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env sh
 
 #
-# script to set a docker environment up
+# script to set a Docker environment up
 #
 
 # fail on any error or undefined variable
 set -eu
 
-# uninstall old versions of docker
+# uninstall old versions of Docker
 sudo apt remove -y docker
 sudo apt remove -y docker-engine
 sudo apt remove -y docker.io
@@ -28,15 +28,18 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 # add Docker repository
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
+# add docker group
+sudo addgroup docker || echo "Ignoring error, group may already exists"
+
+# add current user to docker group (if not, root priviledges are required to run docker commands)
+sudo gpasswd --add $USER docker
+
 # install Docker
 sudo apt update
 sudo apt install -y docker-ce
 sudo apt install -y docker-ce-cli
 sudo apt install -y containerd.io
 sudo apt install -y docker-compose
-
-# add current user to docker group (if not, root priviledges are required to run docker commands)
-sudo usermod -aG docker $USER
 
 # load the group (instead, one can log out and log back in)
 newgrp docker
---
+++ uninstall.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env sh
+
+#
+# script to tear a docker environment down
+#
+
+# fail on any error or undefined variable
+set -eu
+
+# delete user from the docker group
+sudo gpasswd --del $USER docker
+
+# delete docker group
+sudo delgroup docker
+
+# delete Docker packages
+sudo apt purge -y docker-ce
+sudo apt purge -y docker-ce-cli
+sudo apt purge -y containerd.io
+sudo apt purge -y docker-compose
+
+# delete Docker databases
+sudo rm -rf /var/lib/docker
+
+# delete Docker repository
+sudo add-apt-repository --remove "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+# delete Docker GPG key
+sudo apt-key del 0EBFCD88
+
+# don't know if those should be removed, anyone else could use them
+#
+# sudo apt purge -y apt-transport-https
+# sudo apt purge -y ca-certificates
+# sudo apt purge -y curl
+# sudo apt purge -y gnupg-agent
+# sudo apt purge -y software-properties-common
+
+sudo apt autoremove -y
